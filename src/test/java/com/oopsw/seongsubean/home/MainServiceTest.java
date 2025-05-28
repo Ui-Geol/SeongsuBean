aaafca3cf90994b521a858700ef343dd30f554d6
gitbreeder <hayun4362@gmail.com>
2025-05-28 11:27:56 +0900

    chore: DTO필요한 코드 일시적으로 주석처리

--- src/test/java/com/oopsw/seongsubean/home/MainServiceTest.java
+++ src/test/java/com/oopsw/seongsubean/home/MainServiceTest.java
@@ -1,45 +1,44 @@
-package com.oopsw.seongsubean.home;
-import com.oopsw.seongsubean.home.dto.CafeInfo;
-import com.oopsw.seongsubean.home.dto.RankingDto;
-import com.oopsw.seongsubean.home.repository.MainRepository;
-import com.oopsw.seongsubean.home.service.MainService;
-import java.util.List;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class MainServiceTest {
-  @Autowired
-  MainService mainService;
-
-  @Test
-  public void test() {
-    List<CafeInfo> mainCardView = mainService.getMainCardView();
-    System.out.println(mainCardView);
-  }
-
-  @Test
-  public void test2() {
-    List<RankingDto> getRanking = mainService.getRanking();
-    System.out.println(getRanking);
-  }
-
-  @Test
-  public void test3() {
-    String getSearchCafeName = mainService.getSearchCafeName("대림창고");
-    System.out.println(getSearchCafeName);
-  }
-
-  @Test
-  public void test4() {
-    List<String> getSearchCafeMenu = mainService.getSearchCafeMenu("바게트");
-    System.out.println(getSearchCafeMenu);
-  }
-
-  @Test
-  public void test5() {
-    List<String> getEachMenu = mainService.getEachMenu("바게트");
-    System.out.println(getEachMenu);
-  }
-}
+//package com.oopsw.seongsubean.home;
+//import com.oopsw.seongsubean.home.repository.MainRepository;
+//import com.oopsw.seongsubean.home.service.MainService;
+//import java.util.List;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class MainServiceTest {
+//  @Autowired
+//  MainService mainService;
+//
+//
+//  @Test
+//  public void getMainCardViewtest() {
+//    List<CafeInfo> mainCardView = mainService.getMainCardView();
+//    System.out.println(mainCardView);
+//  }
+//
+//  @Test
+//  public void getRankingtest() {
+//    List<RankingDto> getRanking = mainService.getRanking();
+//    System.out.println(getRanking);
+//  }
+//
+//  @Test
+//  public void getSearchCafeNametest() {
+//    String getSearchCafeName = mainService.getSearchCafeName("대림창고");
+//    System.out.println(getSearchCafeName);
+//  }
+//
+//  @Test
+//  public void getSearchCafeMenutest() {
+//    List<String> getSearchCafeMenu = mainService.getSearchCafeMenu("바게트");
+//    System.out.println(getSearchCafeMenu);
+//  }
+//
+//  @Test
+//  public void getEachMenutest() {
+//    List<String> getEachMenu = mainService.getEachMenu("바게트");
+//    System.out.println(getEachMenu);
+//  }
+//}
