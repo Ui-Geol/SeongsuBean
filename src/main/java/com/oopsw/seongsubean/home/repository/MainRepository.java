c8009f42d0673630abaef13b50698df1bf3935f4
gitbreeder <hayun4362@gmail.com>
2025-05-28 19:26:50 +0900

    fix: mybatis연결문제 해결

--- src/main/java/com/oopsw/seongsubean/home/repository/MainRepository.java
+++ src/main/java/com/oopsw/seongsubean/home/repository/MainRepository.java
@@ -1,21 +1,21 @@
-//package com.oopsw.seongsubean.home.repository;
+package com.oopsw.seongsubean.home.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MainRepository {
+
+//  List<CafeInfo> getMainCardView();
 //
-//import java.util.List;
-//import org.apache.ibatis.annotations.Mapper;
-//
-//@Mapper
-//public interface MainRepository {
-//
-////  List<CafeInfo> getMainCardView();
-////
-////  List<RankingDto> getRanking();
-//
-//  String getSearchCafeName(String cafeName);
-//
-//  List<String> getSearchCafeMenu(String menuName);
-//
-//  List<String> getEachMenu(String menuName);
-//
-//}
-//
-//
+//  List<RankingDto> getRanking();
+
+  String getSearchCafeName(String cafeName);
+
+  List<String> getSearchCafeMenu(String menuName);
+
+  List<String> getEachMenu(String menuName);
+
+}
+
+
