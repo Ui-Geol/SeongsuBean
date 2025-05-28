3ea1638a668727f5f2a907b158fd604d0d1c2f7a
gitbreeder <hayun4362@gmail.com>
2025-05-28 17:14:17 +0900

    chore: DTO필요한 코드 일시적으로 주석처리

--- src/main/java/com/oopsw/seongsubean/home/service/MainService.java
+++ src/main/java/com/oopsw/seongsubean/home/service/MainService.java
@@ -1,48 +1,48 @@
-package com.oopsw.seongsubean.home.service;
-
-import com.oopsw.seongsubean.home.repository.MainRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class MainService {
-  private final MainRepository mainRepository;
-
-  /**
-   * 메인 페이지 카드 뷰에 필요한 카페 리스트 조회
-   */
-//  public List<CafeInfo> getMainCardView() {
-//    return mainRepository.getMainCardView();
+//package com.oopsw.seongsubean.home.service;
+//
+//import com.oopsw.seongsubean.home.repository.MainRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+////@Service
+//@RequiredArgsConstructor
+//public class MainService {
+//  private final MainRepository mainRepository;
+//
+//  /**
+//   * 메인 페이지 카드 뷰에 필요한 카페 리스트 조회
+//   */
+////  public List<CafeInfo> getMainCardView() {
+////    return mainRepository.getMainCardView();
+////  }
+////
+////  /**
+////   * 카페 별점 랭킹 리스트 조회
+////   */
+////  public List<RankingDto> getRanking() {
+////    return mainRepository.getRanking();
+////  }
+//
+//  /**
+//   * 카페명으로 정확히 일치하는 카페 이름 조회
+//   */
+//  public String getSearchCafeName(String cafeName) {
+//    return mainRepository.getSearchCafeName(cafeName);
 //  }
 //
 //  /**
-//   * 카페 별점 랭킹 리스트 조회
+//   * 메뉴명으로 해당 메뉴를 팔고 있는 카페 이름들 조회
 //   */
-//  public List<RankingDto> getRanking() {
-//    return mainRepository.getRanking();
+//  public List<String> getSearchCafeMenu(String menuName) {
+//    return mainRepository.getSearchCafeMenu(menuName);
 //  }
-
-  /**
-   * 카페명으로 정확히 일치하는 카페 이름 조회
-   */
-  public String getSearchCafeName(String cafeName) {
-    return mainRepository.getSearchCafeName(cafeName);
-  }
-
-  /**
-   * 메뉴명으로 해당 메뉴를 팔고 있는 카페 이름들 조회
-   */
-  public List<String> getSearchCafeMenu(String menuName) {
-    return mainRepository.getSearchCafeMenu(menuName);
-  }
-
-  /**
-   * 메뉴명을 카테고리처럼 받아, 그 메뉴를 판매하는 카페 주소들 조회
-   */
-  public List<String> getEachMenu(String menuName) {
-    return mainRepository.getEachMenu(menuName);
-  }
-}
+//
+//  /**
+//   * 메뉴명을 카테고리처럼 받아, 그 메뉴를 판매하는 카페 주소들 조회
+//   */
+//  public List<String> getEachMenu(String menuName) {
+//    return mainRepository.getEachMenu(menuName);
+//  }
+//}
