d354efdc1639fbc1902aa61b152ac33a4199d321
goldtwo1989 <ttpd19891213@gmail.com>
2025-05-29 11:49:47 +0900

    feat: Report board Repository 테스트까지 완료

---
+++ src/main/java/com/oopsw/seongsubean/board/dto/ReportBoardDTO.java
@@ -0,0 +1,21 @@
+package com.oopsw.seongsubean.board.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportBoardDTO {
+  private Integer reportBoardId;
+  private String title;
+  private String content;
+  private LocalDate createdDate;
+  private String email;
+  private List<String> images;
+}
