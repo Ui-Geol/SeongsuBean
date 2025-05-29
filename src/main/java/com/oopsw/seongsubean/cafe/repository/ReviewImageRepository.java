b7216884867f54ccd558799d1e47ecf7fe4a025c
Uigeol <gamoon777@naver.com>
2025-05-29 14:31:12 +0900

    feat: review JPA 추가

---
+++ src/main/java/com/oopsw/seongsubean/cafe/repository/ReviewImageRepository.java
@@ -0,0 +1,8 @@
+package com.oopsw.seongsubean.cafe.repository;
+
+import com.oopsw.seongsubean.cafe.domain.ReviewImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewImageRepository extends JpaRepository<ReviewImage, Integer> {
+
+}
