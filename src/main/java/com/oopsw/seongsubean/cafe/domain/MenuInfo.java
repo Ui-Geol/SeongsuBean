80d739f4b3d102e11b097b023385ef5a9ade262d
Uigeol <gamoon777@naver.com>
2025-05-29 20:57:28 +0900

    feat: MenuInfo를 DB와 매칭 및 테스트 준비

--- src/main/java/com/oopsw/seongsubean/cafe/domain/MenuInfo.java
+++ src/main/java/com/oopsw/seongsubean/cafe/domain/MenuInfo.java
@@ -2,6 +2,8 @@ package com.oopsw.seongsubean.cafe.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +20,7 @@ import lombok.ToString;
 public class MenuInfo {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer menuId;
   @Column(nullable = false)
   private String menuCategory;
--- src/test/java/com/oopsw/seongsubean/cafe/repository/MenuInfoRespositoryTest.java
+++ src/test/java/com/oopsw/seongsubean/cafe/repository/MenuInfoRespositoryTest.java
@@ -1,18 +1,32 @@
 package com.oopsw.seongsubean.cafe.repository;
 
-import org.junit.jupiter.api.Test;
+import com.oopsw.seongsubean.config.PropertyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(PropertyConfig.class)
 public class MenuInfoRespositoryTest {
 
   @Autowired
   private MenuInfoRepository menuInfoRepository;
 
-  @Test
-  public void findById() {
-    System.out.println(menuInfoRepository.findById(1));
+//  # 1. 카페 테이블에 데이터 추가(카페 이름, 카페 주소, 카페 상세 주소, 카페 우편번호, 카페 전화번호, 카페 소개, 카폐 대표이미지)
 
-  }
+//  # 2. 카페 이름과 주소를 통해 ID값을 가져오기
+
+//  # 3. 카페 ID 값을 사용하여 영업 시간에 값 삽입
+
+//  6) 상세페이지 메뉴 조회
+
+//  9) 메뉴 수정
+
+//  # 2. 메뉴 업데이트
+
+//  12) 메뉴 삭제
+
+
 }
