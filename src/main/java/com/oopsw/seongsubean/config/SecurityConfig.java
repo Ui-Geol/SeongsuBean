702e02b5e521e8375d76bde6bc3e2ad69ae43f19
mdsihmaro <lsh11946@naver.com>
2025-05-29 10:51:00 +0900

    feat: SecurityConfig 다시 추가 csrf 비활성화 모든 페이지 접근 권한 해제,
    AccountService encoder 복구

---
+++ src/main/java/com/oopsw/seongsubean/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.oopsw.seongsubean.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig {
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable());
+
+    http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    return http.build();
+  }
+}
