df05f3ab5cbf6dded58bbd0012be3e9c03ded28b
mdsihmaro <lsh11946@naver.com>
2025-06-13 14:46:25 +0900

    feat: User Entity로 바꾼거 UserDTO로 수정

--- src/main/java/com/oopsw/seongsubean/auth/AccountDetailsService.java
+++ src/main/java/com/oopsw/seongsubean/auth/AccountDetailsService.java
@@ -1,9 +1,7 @@
 package com.oopsw.seongsubean.auth;
 
-//import com.oopsw.seongsubean.account.dto.UserDTO;
-import com.oopsw.seongsubean.account.domain.UserInfo;
-import com.oopsw.seongsubean.account.repository.jparepository.UserInfoRepository;
-//import com.oopsw.seongsubean.account.repository.mybatisrepository.AccountRepository;
+import com.oopsw.seongsubean.account.dto.UserDTO;
+import com.oopsw.seongsubean.account.repository.mybatisrepository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,12 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AccountDetailsService implements UserDetailsService {
-  private final UserInfoRepository userInfoRepository;
+  private final AccountRepository accountRepository;
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    UserInfo user = userInfoRepository.findByEmail(email)
-            .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email));
+    UserDTO user = accountRepository.findByEmail(email);
+    if (user == null) {
+      throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email);
+    }
     return new AccountDetails(user);
   }
 }
--- src/main/java/com/oopsw/seongsubean/config/SecurityConfig.java
+++ src/main/java/com/oopsw/seongsubean/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.oopsw.seongsubean.config;
 
-import com.oopsw.seongsubean.account.repository.jparepository.UserInfoRepository;
+import com.oopsw.seongsubean.account.repository.mybatisrepository.AccountRepository;
 import com.oopsw.seongsubean.auth.AccountDetailsService;
 import com.oopsw.seongsubean.auth.AccountOauth2UserService;
 import com.oopsw.seongsubean.jwt.JwtAuthorizationFilter;
@@ -13,7 +13,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 
@@ -34,7 +33,7 @@ public class SecurityConfig {
   // 2. SecurityFilterChain 설정
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http,
-                                                 AuthenticationManager authenticationManager, UserInfoRepository userInfoRepository) throws Exception {
+                                                 AuthenticationManager authenticationManager, AccountRepository accountRepository) throws Exception {
 
     http.csrf(csrf -> csrf.disable());
     http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -43,7 +42,7 @@ public class SecurityConfig {
     http.httpBasic(basic -> basic.disable());
     http.addFilter(corsFilter);
     http.addFilter(new JwtAuthorizationFilter(authenticationManager));
-    http.addFilter(new JwtBasicAuthenticationFilter(authenticationManager, userInfoRepository));
+    http.addFilter(new JwtBasicAuthenticationFilter(authenticationManager, accountRepository));
 
     http.authorizeHttpRequests(auth ->
             auth.requestMatchers("/api/v1/user/**").authenticated() //로그인 하면 모두
